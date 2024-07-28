a64896d27f04257fcf12d7d137f0c4fd8e6f2ea8
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-07-28 21:56:27 +0300

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,11 +18,11 @@ public class ExampleJavaMod extends Mod{
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("ты лох");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.cont.button("я крутой(нет) ", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
