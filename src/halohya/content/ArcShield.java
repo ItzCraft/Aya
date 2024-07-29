0dd9aa375840a93b93506b7411c7f1b8e821785c
ItzCraft <147850863+ItzCraft@users.noreply.github.com>
2024-07-29 14:20:05 +0300

    Update ArcShield.java

--- src/halohya/content/ArcShield.java
+++ src/halohya/content/ArcShield.java
@@ -50,5 +50,22 @@ public class ArcShield extends ForceProjector {
         public void updateTile(){
             super.updateTile();
         }
+        @Override
+        public void drawShield(){
+            if(!broken){
+                float radius = realRadius();
+
+                if(radius > 0.001f){
+                    Draw.color(team.color, Color.white, Mathf.clamp(hit));
+
+                    if(renderer.animateShields){
+                        Draw.z(Layer.shields + 0.001f * hit);
+                        Fill.poly(x, y, sides, radius, shieldRotation);
+                    }else{
+                        drawArc(x * tilesize + offset, y * tilesize + offset, radius, angle, player.team().color);
+                   }
+               }
+            }
+        }
     }
 }
